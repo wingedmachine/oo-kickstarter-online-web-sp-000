b40982eeab3fa06ca410a25e8c18fc3228aafc19
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 23:18:24 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,12 @@
 class Backer
-  attr_reader :name
+  attr_reader :name, :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
   end
 end
